445f8b89241b4b309dd50868dfbdc44e6ae4e930
DeepakP <dkp24110@gmail.com>
2017-11-18 22:20:57 +0530

    dkp11

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define boost	ios_base::sync_with_stdio(0);cin.tie(NULL);cout<<fixed<<setprecision(20);
+#define foreach(it,a)		for(__typeof((a).begin()) it=(a).begin();it!=(a).end();it++)
+#define forn(i,n) 			for(int (i)=0;(i)<(int)(n);++(i))
+#define rep(i,a,b)			for(int i=(a),_ed=(b);i<=_ed;i++)
+#define repr(i,a,b)			for(int i=(a),_ed=(b);i>=_ed;i--)
+#define V(T)					vector<T>
+#define P(T1, T2)				pair<T1, T2>
+#define M(T1, T2)				map<T1, T2>
+#define ll						long long int
+#define sz(a)					(int) a.size()
+#define pb						push_back
+#define all(a)					a.begin(), a.end()
+static const int inf = 0x3f3f3f3f, mod = (int)1e9+7;const double eps = 1e-10, pi = acos(-1.0);
+//.......................//.......................(_dkp_).......................//.......................//
+
+//.......................//.......................(_dkp_).......................//.......................//
