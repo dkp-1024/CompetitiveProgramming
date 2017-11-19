c0b0fd348f9301f405261f6d18477a81f31b4e25
DeepakP <dkp24110@gmail.com>
2017-11-19 17:09:44 +0530

    adding VSC tool

--- run_me.sh
+++ run_me.sh
@@ -1,7 +1,7 @@
 git init
 git pull https://github.com/dkp2410/CompetitiveProgramming
 git add .
-git commit -m "dkp11"
+git commit -m "adding VSC tool"
 git remote add origin https://github.com/dkp2410/CompetitiveProgramming
 git remote -v
 git push origin master
